b2cdbab1c9f92eebf850f3463334c65b9b10e6b4
jklgit <jklgog@gmail.com>
2017-05-04 23:15:54 +0200

    iframe instead of link

--- Sankakucomplex_Compare_Images_Github.user.js
+++ Sankakucomplex_Compare_Images_Github.user.js
@@ -8,18 +8,18 @@
 // @grant       none
 // ==/UserScript==
 
-// Add link do deletion page
+// Add frame do deletion page
 var thumbs = document.querySelectorAll('.deleting-post .thumb');
 if(thumbs.length === 2){
-	var a = document.createElement('a');
-	a.innerHTML = 'Compare post content';
-	a.target = '_blank';
-	a.href = 'https://jklgit.github.io/Image-Comparison-in-Browser/index.html\
-		?sid1=' + thumbs[0].getAttribute('id').substr(1) + '&sid2=' + thumbs[1].getAttribute('id').substr(1);
 	
-	var li = document.createElement('li');
-	li.innerHTML = a.outerHTML;
-	document.querySelector('.deleting-post ul').appendChild(li);
+	// Create an iframe
+	var iframe = document.createElement('iframe');
+	iframe.src = 'https://jklgit.github.io/Image-Comparison-in-Browser/index.html\
+		?sid1=' + thumbs[0].getAttribute('id').substr(1) + '&sid2=' + thumbs[1].getAttribute('id').substr(1);
+	iframe.style = 'height:700px; margin-left: 2em; width: calc(100% - 4em);';
+	
+	// Append iframe
+	document.getElementById('content').appendChild(iframe);
 }
 
 // Add link to post/show/ page
